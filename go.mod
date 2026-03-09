3fdfa2f89729621c17127cf4f8b4cd65022bb22e
Felix Goff <felixmgoff@gmail.com>
2026-03-09 14:56:22 +0100

    quick commit, not working

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module Hydroponics/rPi
 
 go 1.25.4
+
+require tinygo.org/x/drivers v0.34.0 // indirect
--- rPi.go
+++ rPi.go
@@ -1,7 +1,58 @@
 package main
 
-import "fmt"
+import (
+	"machine"
+	"net/http"
+	"time"
+
+	"tinygo.org/x/drivers/dht"
+	"tinygo.org/x/drivers/net/link"
+)
+
+var sensor dht.Device
+
+func connectWifi() {
+
+	ssid := "ACAGuest"
+	pass := "FramtidNu"
+
+	println("Connecting to WiFi...")
+	err := link.Connect(ssid, pass)
+	if err != nil {
+		println("Error connecting to WiFi:", err.Error())
+		return
+	}
+	println("Connected to WiFi!")
+}
+
+func readDHT() (int16, int16) {
+	temp, hum, err := sensor.p
+	if err != nil {
+		return -1, -1
+	}
+	return temp, hum
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	t, h := readDHT()
+	w.Write([]byte("temp="))
+	w.Write([]byte(string(t)))
+	w.Write([]byte(" humidity="))
+	w.Write([]byte(string(h)))
+}
 
 func main() {
-	fmt.Println("Hello, RPIPico!")
+
+	// DHT11 setup
+	sensor = dht.New(machine.GPIO15, dht.DHT11)
+
+	// connect wifi
+	connectWifi()
+
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":80", nil)
+
+	for {
+		time.Sleep(time.Hour)
+	}
 }
